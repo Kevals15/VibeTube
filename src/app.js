49f8f4ed1c0f78a5bf317f4db881c9eb9b50d54f
Kevals15 <keval.shah1507@gmail.com>
2025-05-25 15:49:02 +0530

    set all express setting installation of cors and cookieParser

--- src/app.js
+++ src/app.js
@@ -1,1 +1,15 @@
-ECHO is on.
+import express from 'express'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+const app = express()
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+})) // Usage of whitelisting urls from this origin
+
+app.use(express.json({ limit: "16kb" }))//convert data into json
+app.use(express.urlencoded({ limit: "16kb", extended: true })) //for taking data from url
+app.use(express.static('public')) // All assests are puting inside public folder it served static folder 
+app.use(cookieParser())
+export { app }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import connectDb from "../db/index.js";
 import dotenv from 'dotenv'
-
+import app from './app.js'
 
 dotenv.config({
     path: './env'
 })
 
 connectDb()
+    .then(() => {
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(` Server listening at port ${process.env.PORT}`);
+        })
+    })
+    .catch((err) => {
+        console.log(`DB CONNECTION FAILED ${err}`);
+    })
